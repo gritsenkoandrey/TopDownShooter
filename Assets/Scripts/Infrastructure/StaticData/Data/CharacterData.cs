d28af4a3e3717e16ad687bb9792d8b641753cc1f
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2024-01-13 22:46:21 +0300

    feat: refactoring.

--- Assets/Scripts/Infrastructure/StaticData/Data/CharacterData.cs
+++ Assets/Scripts/Infrastructure/StaticData/Data/CharacterData.cs
@@ -7,10 +7,7 @@ namespace CodeBase.Infrastructure.StaticData.Data
     public sealed class CharacterData : ScriptableObject
     {
         [Range(1, 20)] public int Health;
-        [Range(1, 100)] public int Damage;
         [Range(1f, 5f)] public float Speed;
-        [Range(2.5f, 10f)] public float AttackDistance;
-        [Range(0.1f, 2f)] public float AttackRecharge;
         public AssetReference PrefabReference;
     }
 }
